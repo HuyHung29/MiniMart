675c5b7003d5f8e4710675976c5a7509dac6be05
HuyHung <lehuyhugn29@gmail.com>
2022-08-09 18:45:52 +0700

    change src file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Layout from "Layout";
+import "slick-carousel/slick/slick.css";
+import Layout from "app";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
